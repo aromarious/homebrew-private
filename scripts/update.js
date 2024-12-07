1915925cfaf64fa56f16a5fdbdcdfad26459e94e
aromarious <6535448+aromarious@users.noreply.github.com>
2024-12-07 19:26:10 +0900

    feat: new formula sidecar-launcher

--- scripts/update.js
+++ scripts/update.js
@@ -47,25 +47,4 @@ const main = ({
 
 const [, , formula, description, homepage, url, sha256, version, license] =
   process.argv
-  console.log(process.argv)
-  // console.log({
-  //   formula,
-  //   description,
-  //   homepage,
-  //   url,
-  //   sha256,
-  //   version,
-  //   license,
-  // })
 main({ formula, description, homepage, url, sha256, version, license })
-// commandline example:
-/*
-FORMULA="display-rotate" \
-DESCRIPTION='Rotate your display easily via command-line on macOS using displayplacer.' \
-HOMEPAGE='https://github.com/aromarious/display-rotate' \
-URL='https://github.com/aromarious/display-rotate/releases/download/v1.0.0/display-rotate-1.0.2.tar.gz' \
-SHA256='26e6597cff5eb6e98362e362b9ea0cc79c17be2d4685e6994a8b99f9fd755ae8' \
-VERSION='v1.0.2' \
-LICENSE='ISC' \
-./scripts/update.js "$FORMULA" "$DESCRIPTION" "$HOMEPAGE" "$URL" "$SHA256" "$VERSION" "$LICENSE"
-*/
