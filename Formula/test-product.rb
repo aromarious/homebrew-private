b92ad48dec9522b7b97fdd1187e9a9d0b8b488c9
aromarious <aromarious@users.noreply.github.com>
2024-12-08 22:38:23 +0000

    Update packages

---
+++ Formula/test-product.rb
@@ -0,0 +1,22 @@
+# 12/8/2024, 10:38:23 PM
+class TestProduct < Formula
+  desc "test product decsciption"
+  homepage "https://github.com/aromarious/display-rotate"
+  url "https://github.com/aromarious/display-rotate/releases/download/v1.0.0/display-rotate-1.0.2.tar.gz"
+  sha256 "26e6597cff5eb6e98362e362b9ea0cc79c17be2d4685e6994a8b99f9fd755ae8"
+  version "v1.0.2"
+  license "ISC"
+
+  depends_on "node"
+  depends_on "displayplacer"
+
+  def install
+    ohai "Installing test-product..."
+    libexec.install "package.json", "package-lock.json", "bin", "dist"
+    bin.install_symlink libexec/"bin/test-product"
+  end
+
+  test do
+    system "#{bin}/test-product", "--version"
+  end
+end
