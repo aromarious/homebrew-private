7f15c14af9452f9bcb01951b0cdb175111cc240e
aromarious <6535448+aromarious@users.noreply.github.com>
2024-12-16 15:58:06 +0900

    Update packages for display-rotate for v1.1.0 (#30)
    Co-authored-by: aromarious <aromarious@users.noreply.github.com>

--- Formula/display-rotate.rb
+++ Formula/display-rotate.rb
@@ -1,10 +1,10 @@
-# 12/9/2024, 2:02:20 AM
+# 12/16/2024, 6:56:17 AM
 class DisplayRotate < Formula
   desc "Rotate your display easily on macOS CLI using displayplacer."
   homepage "https://github.com/aromarious/display-rotate"
-  url "https://github.com/aromarious/display-rotate/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "778b2be4dbc5149c639576cf752b944508f5098142982beac6f6e54b3f7118df"
-  version "v1.0.5"
+  url "https://github.com/aromarious/display-rotate/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "b2b596b127be2eb7d4a36034941758e379ab33ce2b03f2a76f3ce533e88f1017"
+  version "v1.1.0"
   license "ISC"
 
   depends_on "node"
